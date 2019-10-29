767090d769bc0ee73e2d81a8dc82c7a2adb532d9
changxianglin <281532612@qq.com>
2019-10-29 20:54:54 +0800

    finish basic rest api

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
-
 const app = new express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
 
 app.get('/', (req, res) => {
   res.send('We are on home')
 })
 
-app.post('/posts', (req, res) => {
-  ses.send('We are on posts')
-})
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true,  useUnifiedTopology: true  }, () => console.log('connected to DB!'))
 
 app.listen(3000)
